1b48c05002a1b73efefaeeb7cf4e7e7b3d32bc57
MrWilliamA <will.archer@hotmail.com>
2022-03-03 20:49:34 +1100

    fin

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const element = document.createElement('div'); 
+document.body.appendChild(element); 
+
+element.style.fontSize = '24px';
+element.style.marginLeft = '30px';
+element.style.lineHeight = 2; 
+element.style.background = 'grey';
+element.style.height = '100px';
+
+element.className = "dog";
+element.classList.add("pet-listing", "doggy"); 
